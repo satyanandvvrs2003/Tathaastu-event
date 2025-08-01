454809872fe32c5ca62b051bcd29cfe3052dd1d0
satyanandvvrs2003 <satyanandvvrs@gmail.com>
2025-08-01 22:41:33 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyCj6rSxY--VUWioNHIHrw8kzf7C2i-f990",
+  authDomain: "thaastu-events.firebaseapp.com",
+  databaseURL: "https://thaastu-events-default-rtdb.firebaseio.com",
+  projectId: "thaastu-events",
+  storageBucket: "thaastu-events.appspot.com",
+  messagingSenderId: "1044759719995",
+  appId: "1:1044759719995:web:c02253409e40b36c2aec1e"
+};
+
+firebase.initializeApp(firebaseConfig);
